d761c001eb23dad1b2edc8a779e92a1a8817ee46
Ajay Jakate <ajay.jakate@gmail.com>
2023-02-22 00:58:40 -0600

    adds reagent; renders simple component

--- src/main/masterbeat/app.cljs
+++ src/main/masterbeat/app.cljs
@@ -1,4 +1,13 @@
-(ns masterbeat.app)
+(ns masterbeat.app
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
 
-(defn init []
-  (println "Hello World"))
+(defn simple-component []
+  [:div
+   [:p "I am nothing"]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
+(defn ^:dev/after-load init []
+  (rdom/render [simple-component] (js/document.getElementById "root")))
